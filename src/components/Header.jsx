b2f0532fa5200c54a00373c2d2b2b9d2d1833b0f
Mohamedreda03 <mo03.reda@gmail.com>
2024-02-15 17:18:10 +0200

    deploy message

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,104 +10,101 @@ import { BiMenuAltRight } from "react-icons/bi";
 import MobileMenu from "./MobileMenu";
 
 const Header = () => {
-    const [show, setShow] = useState("translate-y-0");
-    const [showCatMenu, setShowCatMenu] = useState(false);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [lastScrollY, setLastScrollY] = useState(4);
+  const [show, setShow] = useState("translate-y-0");
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(4);
 
-    // ..............................................................................
+  // ..............................................................................
 
-    const controlNavbar = () => {
-        if (window.scrollY > 200) {
-            if (window.scrollY > lastScrollY && !mobileMenu) {
-                setShow("-translate-y-[80px]");
-            } else {
-                setShow("shadow-sm");
-            }
-        } else {
-            setShow("translate-y-0");
-        }
-        // ترتيب الاحداث هنا هو ال مخلي قيمت دي اكبر من الي فوق
-        setLastScrollY(window.scrollY);
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("-translate-y-[80px]");
+      } else {
+        setShow("shadow-sm");
+      }
+    } else {
+      setShow("translate-y-0");
+    }
+    // ترتيب الاحداث هنا هو ال مخلي قيمت دي اكبر من الي فوق
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
     };
+  }, [lastScrollY]);
 
-    useEffect(() => {
-        window.addEventListener("scroll", controlNavbar);
+  // ..............................................................................
 
-        return () => {
-            window.removeEventListener("scroll", controlNavbar);
-        };
-    }, [lastScrollY]);
-
-    // ..............................................................................
-
-    return (
-        <header
-            className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between
+  return (
+    <header
+      className={`w-full h-[70px] md:h-[80px] bg-white flex items-center justify-between
        z-20 sticky top-0 transition-transform duration-300 ${show}`}
-        >
-            <Wrapper className="flex justify-between items-center h-[60px]">
-                <Link to={"/"}>
-                    <img src={logo} alt="" className="w-[40px] md:w-[60px]" />
-                </Link>
-                <Menu
-                    showCatMenu={showCatMenu}
-                    setShowCatMenu={setShowCatMenu}
-                />
-                {/* ............................ */}
-                {mobileMenu && (
-                    <MobileMenu
-                        showCatMenu={showCatMenu}
-                        setShowCatMenu={setShowCatMenu}
-                        setMobileMenu={setMobileMenu}
-                    />
-                )}
+    >
+      <Wrapper className="flex justify-between items-center h-[60px]">
+        <Link to={"/"}>
+          <img src={logo} alt="" className="w-[40px] md:w-[60px]" />
+        </Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+        {/* ............................ */}
+        {mobileMenu && (
+          <MobileMenu
+            showCatMenu={showCatMenu}
+            setShowCatMenu={setShowCatMenu}
+            setMobileMenu={setMobileMenu}
+          />
+        )}
 
-                {/* ............................ */}
-                <div className="flex items-center gap-2 text-black">
-                    {/* icon start */}
-                    <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex items-center justify-center hover:bg-black/[0.05] cursor-pointer relative">
-                        <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
-                        <div
-                            className="absolute top-1 left-5 md:left-7 text-[10px] md:text-[12px] rounded-full bg-red-600 h-[14px] md:h-[18px] 
+        {/* ............................ */}
+        <div className="flex items-center gap-2 text-black">
+          {/* icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex items-center justify-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+            <div
+              className="absolute top-1 left-5 md:left-7 text-[10px] md:text-[12px] rounded-full bg-red-600 h-[14px] md:h-[18px] 
                             min-w-[14px] md:min-w-[18px] flex justify-center items-center text-white px-[2px] md:px-[5px]"
-                        >
-                            51
-                        </div>
-                    </div>
-                    {/* icon end */}
-                    <Link to={"/productcar"}>
-                        <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex items-center justify-center hover:bg-black/[0.05] cursor-pointer relative">
-                            <BsCart className="text-[15px] md:text-[20px]" />
-                            <div
-                                className="absolute top-1 left-5 md:left-7 text-[10px] md:text-[12px] rounded-full bg-red-600 h-[14px] md:h-[18px] 
+            >
+              51
+            </div>
+          </div>
+          {/* icon end */}
+          <Link to={"/productcar"}>
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex items-center justify-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart className="text-[15px] md:text-[20px]" />
+              <div
+                className="absolute top-1 left-5 md:left-7 text-[10px] md:text-[12px] rounded-full bg-red-600 h-[14px] md:h-[18px] 
                             min-w-[14px] md:min-w-[18px] flex justify-center items-center text-white px-[2px] md:px-[5px]"
-                            >
-                                5
-                            </div>
-                        </div>
-                    </Link>
-                    {/* icon end */}
-                    <div
-                        className="w-8 md:w-12 h-8 md:h-12 rounded-full flex items-center justify-center
+              >
+                5
+              </div>
+            </div>
+          </Link>
+          {/* icon end */}
+          <div
+            className="w-8 md:w-12 h-8 md:h-12 rounded-full flex items-center justify-center
                          hover:bg-black/[0.05] cursor-pointer relative -mr-2 md:hidden"
-                    >
-                        {mobileMenu ? (
-                            <VscChromeClose
-                                className="text-[19px]"
-                                onClick={() => setMobileMenu(false)}
-                            />
-                        ) : (
-                            <BiMenuAltRight
-                                className="text-[24px]"
-                                onClick={() => setMobileMenu(true)}
-                            />
-                        )}
-                    </div>
-                </div>
-            </Wrapper>
-        </header>
-    );
+          >
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[19px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[24px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+        </div>
+      </Wrapper>
+    </header>
+  );
 };
 
 export default Header;
