a7a12b769e738c8e49f3a1997ef4b0e09ae3cb53
Mohamedreda03 <mo03.reda@gmail.com>
2024-02-15 17:31:20 +0200

    fdjklj

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,27 @@ import Category from "./pages/category/Category";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProductDetails from "./pages/ProductDetails";
-import ShopingCar from "./pages/shopingCar";
 import Failed from "./pages/Failed";
 import Success from "./pages/Success";
+import ShopingCar from "./pages/ShopingCar";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <div className="min-h-[100vh]">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="category" element={<Category />} />
-                    <Route path="productdetails" element={<ProductDetails />} />
-                    <Route path="productcar" element={<ShopingCar />} />
-                    <Route path="productcar/failed" element={<Failed />} />
-                    <Route path="productcar/success" element={<Success />} />
-                </Routes>
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <div className="min-h-[100vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="category" element={<Category />} />
+          <Route path="productdetails" element={<ProductDetails />} />
+          <Route path="productcar" element={<ShopingCar />} />
+          <Route path="productcar/failed" element={<Failed />} />
+          <Route path="productcar/success" element={<Success />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/ShopingCar.jsx
+++ src/pages/ShopingCar.jsx
@@ -5,51 +5,49 @@ import imgEmpty from "../assets/empty-cart.jpg";
 import { Link } from "react-router-dom";
 
 const ShopingCar = () => {
-    return (
-        <div className="w-full md:p-20">
-            <Wrapper>
-                <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
-                    <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-                        Shopping Cart
-                    </div>
+  return (
+    <div className="w-full md:p-20">
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Shopping Cart
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row gap-12 py-10">
+          <div className="flex-[2]">
+            <div className="text-lg font-bold">Cart Item</div>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </div>
+          <div className="flex-[1]">
+            <div className="text-lg font-bold">Summary</div>
+            <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
+              <div className="flex justify-between">
+                <div className="uppercase text-md md:text-lg font-medium text-black">
+                  Subtotal
                 </div>
-                <div className="flex flex-col md:flex-row gap-12 py-10">
-                    <div className="flex-[2]">
-                        <div className="text-lg font-bold">Cart Item</div>
-                        <CartItem />
-                        <CartItem />
-                        <CartItem />
-                        <CartItem />
-                        <CartItem />
-                    </div>
-                    <div className="flex-[1]">
-                        <div className="text-lg font-bold">Summary</div>
-                        <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
-                            <div className="flex justify-between">
-                                <div className="uppercase text-md md:text-lg font-medium text-black">
-                                    Subtotal
-                                </div>
-                                <div className="text-md md:text-lg font-medium">
-                                    $ 19 695.00
-                                </div>
-                            </div>
-                            <div className="text-sm md:text-md py-5 border-t mt-5">
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Aliquid quae quidem mollitia
-                                eius. Debitis a voluptatibus omnis corrupti
-                                repellendus. Saepe atque totam eaque veritatis
-                                deleniti voluptates maxime iure ut excepturi,
-                                eveniet at expedita aliquam reiciendis illum sed
-                                iste? Dolorum, qui!
-                            </div>
-                        </div>
-                        <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">
-                            Checkout
-                        </button>
-                    </div>
+                <div className="text-md md:text-lg font-medium">
+                  $ 19 695.00
                 </div>
-                {/* this is empty car */}
-                {/* <div className="flex-[2] flex flex-col items-center pb-[50px] md:0mt-14">
+              </div>
+              <div className="text-sm md:text-md py-5 border-t mt-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+                quae quidem mollitia eius. Debitis a voluptatibus omnis corrupti
+                repellendus. Saepe atque totam eaque veritatis deleniti
+                voluptates maxime iure ut excepturi, eveniet at expedita aliquam
+                reiciendis illum sed iste? Dolorum, qui!
+              </div>
+            </div>
+            <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">
+              Checkout
+            </button>
+          </div>
+        </div>
+        {/* this is empty car */}
+        {/* <div className="flex-[2] flex flex-col items-center pb-[50px] md:0mt-14">
                     <img
                         src={imgEmpty}
                         alt="img"
@@ -71,9 +69,9 @@ const ShopingCar = () => {
                         </button>
                     </Link>
                 </div> */}
-            </Wrapper>
-        </div>
-    );
+      </Wrapper>
+    </div>
+  );
 };
 
 export default ShopingCar;
