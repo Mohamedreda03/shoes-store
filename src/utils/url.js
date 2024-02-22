856ea80b1e35f9c66a33f647ef1bc828694f7d25
Mohamedreda03 <mo03.reda@gmail.com>
2024-02-22 14:00:27 +0200

    cc

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,3 @@
 export const STRAPI_API_TOKEN = import.meta.env.VITE_STRAPI_API_TOKEN;
 
-export const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5500";
+export const API_URL = import.meta.env.API_URL || "http://127.0.0.1:5500";
